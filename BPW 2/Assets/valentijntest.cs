661e55bc0c25bf75145382c260885e154a91012e
Inge Praat <>
2019-06-19 19:31:16 +0200

    beginning of state machine!

---
+++ BPW 2/Assets/valentijntest.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EnemyStates { Idle = 0, Attack = 1, Patrol = 2}
+
+public class valentijntest : MonoBehaviour
+{
+
+    public EnemyStates state;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = EnemyStates.Idle;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ExecuteStates();
+    }
+
+    void ExecuteStates()
+    {
+        switch (state)
+        {
+            case EnemyStates.Attack:
+
+                break;
+
+            case EnemyStates.Idle:
+
+                //idle code
+
+                //if(condition)
+                //{
+                    //state = EnemyStates.Attack;
+                //}
+
+                break;
+
+            case EnemyStates.Patrol:
+                break;
+
+        }
+    }
+}
