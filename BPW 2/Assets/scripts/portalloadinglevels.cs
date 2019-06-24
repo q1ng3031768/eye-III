75a68df00a02618ddcce45358eeaed7fcd52cdf3
Inge Praat <>
2019-06-24 23:38:28 +0200

    final final final!!

---
+++ BPW 2/Assets/scripts/portalloadinglevels.cs
@@ -0,0 +1,13 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class portalloadinglevels : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
